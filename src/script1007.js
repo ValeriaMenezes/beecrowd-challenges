e1cfccee82fd668736e35d80715b07ff6520b75e
Valéria Menezes de Souza <valeriamenezes022@gmail.com>
2023-07-09 01:07:39 -0300

    adiciona desafio 1007

---
+++ src/script1007.js
@@ -0,0 +1,11 @@
+const input = require('./initialDefault');
+const lines = input.split('\n');
+
+const A = parseInt(lines.shift());
+const B = parseInt(lines.shift());
+const C = parseInt(lines.shift());
+const D = parseInt(lines.shift());
+
+const diferenca = (A * B) - (C * D);
+const message = `DIFERENCA = ${diferenca}`;
+console.log(message);
