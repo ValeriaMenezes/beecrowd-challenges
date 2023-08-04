a89b30b84f2354f25610be6498e387f808531c2a
Valéria <valeriamenezes022@gmail.com>
2023-08-03 22:00:35 -0300

    adiciona desafio 1035

---
+++ src/script1035.js
@@ -0,0 +1,16 @@
+// Teste de Seleção 1 - Beecrowd 1035
+
+const input = require('./scriptMain');
+const lines = input.split(' ');
+
+const A = parseInt(lines.shift());
+const B = parseInt(lines.shift());
+const C = parseInt(lines.shift());
+const D = parseInt(lines.shift());
+
+
+if (B > C && D > A && (C + D > A + B) && C > 0 && D > 0 && A % 2 === 0) {
+  console.log('Valores aceitos');
+} else {
+  console.log('Valores nao aceitos');
+}
