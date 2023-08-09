f61105f2f57244502ee9d1dbd908f657effb9e37
Valéria <valeriamenezes022@gmail.com>
2023-08-09 12:24:41 -0300

    corrige desafio 1042

--- src/script1042.js
+++ src/script1042.js
@@ -4,31 +4,45 @@ const input = require('./scriptMain');
 const valuesArray = input.split(' ').map((num) => Number(num));
 
 const sortNumbers = (valuesArray) => {
-  const copy = valuesArray.slice();
+  const copyOriginals = valuesArray.slice();
   
   for (let extIndex = 0; extIndex < valuesArray.length; extIndex += 1) {
     for (let intIndex = 0; intIndex < valuesArray.length - 1 - extIndex; intIndex += 1) {
       if (valuesArray[intIndex] > valuesArray[intIndex + 1]) {
-        let temp = valuesArray[intIndex]
+        let temp = valuesArray[intIndex];
         valuesArray[intIndex] = valuesArray[intIndex + 1];
         valuesArray[intIndex + 1] = temp;
       }
     }
   }
 
-  // valuesArray.forEach((num) => console.log(num));
-  // console.log(' ');
-  // copy.forEach((i) => console.log(i));
-
-  for (let valor of valuesArray) {
-    console.log(valor);
-  }
-
-  console.log(' ');
-
-  for (let valor of copy) {
-    console.log(valor);
-  }
+  const message = `${valuesArray.join('\n')}\n\n${copyOriginals.join('\n')}`;
+  return message;
 }
 
-sortNumbers(valuesArray);
+console.log(sortNumbers(valuesArray));
+
+// A SOLUÇÃO ACIMA NÃO DEPENDE DE APENAS TRÊS VALORES DE ENTRADA E POR ISSO É MAIS DINÂMICA QUE A SOLUÇÃO ABAIXO
+
+// const sortNumbers = (valuesArray) => {
+//   const copy = valuesArray.slice();
+  
+//   for (let extIndex = 0; extIndex < valuesArray.length; extIndex += 1) {
+//     for (let intIndex = 0; intIndex < valuesArray.length - 1 - extIndex; intIndex += 1) {
+//       if (valuesArray[intIndex] > valuesArray[intIndex + 1]) {
+//         let temp = valuesArray[intIndex]
+//         valuesArray[intIndex] = valuesArray[intIndex + 1];
+//         valuesArray[intIndex + 1] = temp;
+//       }
+//     }
+//   }
+
+ 
+//   const [value1, value2, value3] = valuesArray;
+//   const [original1, original2, original3] = copy;
+  
+//   const message = `${value1}\n${value2}\n${value3}\n\n${original1}\n${original2}\n${original3}`
+//   return message;
+// }
+
+// console.log(sortNumbers(valuesArray));
