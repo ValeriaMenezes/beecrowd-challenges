af7a60a716280df1b439f9488e3b92b5abc2370c
Valéria <valeriamenezes022@gmail.com>
2023-08-28 11:51:27 -0300

    adiciona desafio 1070

---
+++ src/script1070.js
@@ -0,0 +1,19 @@
+// Beecrowd 1068 - Seis Números Ímpares
+
+const input = require('./scriptMain');
+let number = Number(input);
+
+if (number % 2 === 0) {
+  number += 1; // se for par, somamos com 1 para se tornar ímpar
+  // console.log(number);
+  for (let value = 0; value < 6; value += 1) {
+    // a cada uma das seis iterações somamos com 2 para sempre termos ímpares
+    console.log(number);
+    number += 2;
+  }
+} else if (number % 2 !== 0) {
+    for (let value = 0; value < 6; value += 1) {
+      console.log(number);
+      number += 2;
+  }
+}
