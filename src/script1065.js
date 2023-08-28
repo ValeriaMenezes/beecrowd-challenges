c985638ff431b1617f1ee13ddb4f4f1551d72732
Valéria <valeriamenezes022@gmail.com>
2023-08-28 10:50:23 -0300

    corrige desafio 1065

--- src/script1065.js
+++ src/script1065.js
@@ -3,12 +3,10 @@
 const input = require('./scriptMain');
 const numbers = input.split('\n').map((num) => Number(num));
 
-console.log('aquii', numbers);
-
 const pairsFiveNumbers = (numbers) => {
   let count = 0;
 
-  for (let index = 0; index < numbers.length; index += 1) {
+  for (let index = 0; index < numbers.length -1; index += 1) {
     if (numbers[index] % 2 === 0) {
       count += 1;
     }
---
+++ src/script1066.js
@@ -0,0 +1,58 @@
+// Beecrowd 1066 - Pares, Ímpares, Positivos e Negativos
+
+const input = require('./scriptMain');
+const numbers = input.split('\n').map((num) => Number(num));
+
+const verifyNumbers = (numbers) => {
+  let positive = 0;
+  let negative = 0;
+  let pair = 0;
+  let odd = 0;
+
+  for (let index = 0; index < numbers.length -1; index += 1) {
+    let number = numbers[index];
+
+    if (number % 2 === 0) {
+      pair += 1;
+    } else {
+      odd += 1;
+    }
+
+    if (number > 0) {
+      positive += 1;
+    } else if (number < 0) {
+      negative += 1;
+    }
+  }
+
+  const message = `${pair} valor(es) par(es)
+${odd} valor(es) impar(es)
+${positive} valor(es) positivo(s)
+${negative} valor(es) negativo(s)`;
+
+  return message;
+}
+
+console.log(verifyNumbers(numbers));
+
+
+// positivo 
+//  -> par ou impar
+// negativo
+//  -> par ou impar
+
+    // if (number > 0) {
+    //   positive += 1;
+    //   if (number % 2 === 0) {
+    //     pair += 1;
+    //   } else {
+    //     odd += 1;
+    //   }
+    // } else {
+    //   negative += 1;
+    //   if (number % 2 === 0) {
+    //     pair += 1;
+    //   } else {
+    //     odd += 1;
+    //   }
+    // }
