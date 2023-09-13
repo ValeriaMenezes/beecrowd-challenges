4646590cfe79454b1069e7e34063f12f9ed17c62
Valéria <valeriamenezes022@gmail.com>
2023-09-13 15:07:16 -0300

    adiciona desafio 1131

--- src/script1118.js
+++ src/script1118.js
@@ -41,3 +41,38 @@ for (let i = 0; i < lines.length; i += 1) {
     accNotes = 0;
   }
 }
+
+// Loop for:
+
+// Inicia um loop que percorre cada elemento do array lines (linhas da entrada).
+// let note = lines[i];
+
+// Atribui o valor da linha atual do array lines à variável note.
+// if (note >= 0 && note <= 10) {
+
+// Verifica se note está dentro do intervalo válido de notas (entre 0 e 10).
+// accNotes += 1;
+
+// Incrementa accNotes para rastrear o número de notas válidas lidas.
+// if (accNotes === 1) {
+
+// Se esta é a primeira nota válida, atribui o valor de note a n1.
+// else if (accNotes === 2) {
+
+// Se esta é a segunda nota válida, atribui o valor de note a n2 e calcula a média das duas notas.
+// console.log(media = ${average.toFixed(2)});
+
+// Imprime a média calculada com duas casas decimais.
+// if (accNotes > 2 && note === 2) {
+
+// Se já foram lidas duas notas e o usuário inseriu 2, o programa exibe a mensagem "novo calculo (1-sim 2-nao)" e retorna, encerrando o programa.
+// Verificações de notas inválidas:
+
+// As próximas duas if verifica se a nota é inválida (menor que 0 ou maior que 10) e imprime "nota invalida" se for o caso.
+// if (accNotes > 2) {
+
+// Se já foram lidas mais de duas notas, o programa exibe a mensagem "novo calculo (1-sim 2-nao)".
+// if (note === 1 && accNotes > 2) {
+
+// Se o usuário inserir 1 e já foram lidas mais de duas notas, o programa redefine accNotes para 0, permitindo um novo cálculo de média.
+// Em resumo, este código lê notas, calcula a média quando duas notas válidas são fornecidas e lida com as mensagens de "novo calculo (1-sim 2-nao)" de acordo com a lógica do problema.
