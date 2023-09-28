ed5bd88c0486bc2a1eae802cb258ed1d8a682758
Valéria <valeriamenezes022@gmail.com>
2023-09-28 19:56:49 -0300

    adiciona desafio 1186

--- iniciante/script1185.js
+++ iniciante/script1185.js
@@ -1,4 +1,4 @@
-// Beecrowd 1184 - Abaixo da Diagonal Principal
+// Beecrowd 1185 - Acima da Diagonal Secundária
 
 const input = require('./scriptMain');
 const lines = input.split('\n');
@@ -8,10 +8,10 @@ const letter = lines.shift();
 let count = 0;
 let sum = 0;
 
-for (let l = 0; l < 12; l += 1) {
+for (let l = 11; l >= 0; l -= 1) {
   for (let c = 0; c < 12; c += 1) {
-    const value = parseFloat(lines.shift());
-    if (c < l) { // só está abaixo da diagonal principal os números cuja coluna é menor que a linha
+    const value = parseFloat(lines[12 * l + c]);
+    if (c < (11 - l)) { // só está acima da diagonal secundária os números cuja coluna é menor que a linha menos 11
       count += 1;
       sum += value;
     }
